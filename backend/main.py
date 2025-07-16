7929d6d670437161ad9ee46e31cd8dcf5cd50ca4
Jefferson Vega <99852874+jeffersonvegag@users.noreply.github.com>
2025-07-15 23:12:15 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173","https://app-ml-tesis-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
